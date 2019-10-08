3b7ba33dcaf36e1fedbdbfe7769546ac9fe8551d
TyIL TTY7 <q1342893971@gmail.com>
2019-10-09 01:12:36 +0800

    [function] improve progress indication

--- better_douban.py
+++ better_douban.py
@@ -21,8 +21,8 @@ class Douban_Movie_Entry(object):
     def __repr__(self):
         title = '\'{}\''.format(self.title) if (self.title != '') else 'EMPTY'
         release_date = '\'{}\''.format(self.release_date) if (self.release_date != '') else 'EMPTY'
-        return '<class: \'{class_}\' at {id}; title: {title}, release_date: {release_date}>'.format(
-            class_ = self.__class__.__name__, id = hex(id(self)),
+        return '<\'{class_}\'; title: {title}, release_date: {release_date}>'.format(
+            class_ = self.__class__.__name__,
             title = title, release_date = release_date,
         )
 
@@ -42,11 +42,13 @@ def fill_list(list_: list, list_type: str, people_id: str):
         # extract the item list in the page
         for item in page_soup.find_all('li', class_ = 'item'):
             # extract item info from list elements
-            entry = item.find('a')
-            list_.append(Douban_Movie_Entry(
-                title = entry.string.strip(),
-                link = entry['href'],
-            ))
+            entry_property = item.find('a')
+            entry = Douban_Movie_Entry(
+                title = entry_property.string.strip(),
+                link = entry_property['href'],
+            )
+            list_.append(entry)
+            print('ENTRY ADDED: {}'.format(repr(entry)))
         # check if there is 'next page'
         next_page_link = page_soup.find('span', class_ = 'next').find('a')
         if (next_page_link is not None):
@@ -56,14 +58,15 @@ def fill_list(list_: list, list_type: str, people_id: str):
             break
 
 def inspect_list(list_: List[Douban_Movie_Entry]):
-    for item in list_:
-        page = requests.get(item.link)
+    for entry in list_:
+        page = requests.get(entry.link)
         page_soup = BeautifulSoup(page.text, 'html.parser')
         entry_info = page_soup.find('div', id = 'info')
         # TODO: if there exist multiple dates, should first sort them
-        item.release_date = ' / '.join([
+        entry.release_date = ' / '.join([
             date.string.strip() for date in entry_info.find_all('span', property = 'v:initialReleaseDate')
         ])
+        print('ENTRY DETAIL ADDED: {}'.format(repr(entry)))
 
 def main():
     people_id = input('your personal douban id: ')
