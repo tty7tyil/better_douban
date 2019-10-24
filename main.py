8b23d62227cc83b18be713e29804453909c39cdc
TyIL TTY7 <q1342893971@gmail.com>
2019-10-24 23:15:54 +0800

    [trivial] easier to print entry list in command line

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ START_URL = 'https://movie.douban.com/people/{people_id}/{list_type}?start=0&sor
 import sensitive_info
 # set up sensitivy personal information
 
+list_wish = None
+
+def print_list(list_: dmel.Douban_Movie_Entry_List) -> None:
+    for e in list_:
+        print('{}{}'.format(
+            mua.mixed_unicode_align('.', '<', 80,
+            e.get_title(), resolve_as_wide = ['\u2026']
+        ),
+            e.get_release_date()
+        ))
+
 def main():
+    global list_wish
+
     list_wish = dmel.Douban_Movie_Entry_List(
         start_url = START_URL.format(
             people_id = sensitive_info.people_id,
@@ -29,13 +42,7 @@ def main():
     list_wish.fill_list()
     list_wish.inspect_list()
 
-    for e in list_wish:
-        print('{}{}'.format(
-            mua.mixed_unicode_align('.', '<', 80,
-            e.get_title(), resolve_as_wide = ['\u2026']
-        ),
-            e.get_release_date()
-        ))
+    print_list(list_wish)
 
 if (__name__ == '__main__'):
     main()
